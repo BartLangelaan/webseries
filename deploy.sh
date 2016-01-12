14cb6b5105125955ed23f6b904705db2a057cd07
Bart Langelaan <langelaanbart@gmail.com>
2016-01-12 05:22:00 +0100

    I have no idea what i'm doing

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+echo "DEPLOYING"
+echo $METEOR_USER
 
-printf '$METEOR_USER\n$METEOR_PASS\n' | meteor login
+printf '$METEOR_USER\n$METEOR_PASS\n'| meteor login
 cd src
 meteor deploy wildspot.meteor.com
